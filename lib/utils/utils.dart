bd62e0786309f5c8f5f156c0eeb608ff4b93a927
harshil-micrasol <harshil.micrasol@gmail.com>
2023-07-05 12:38:49 +0530

    MaterialButton, Icon button, Loaders and utils added

---
+++ lib/utils/utils.dart
@@ -0,0 +1,7 @@
+const double defaultPadding = 18.0;
+const double defaultRadius = 8.0;
+
+bool isValEmpty(dynamic val) {
+  String? value = val.toString();
+  return (val == null || value.isEmpty || value == "null" || value == "" || value == "NULL");
+}
